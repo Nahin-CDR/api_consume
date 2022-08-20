35d57235e22cac1d765119ec04d9260288429ac1
Nahin-CDR <dev.nahin95@gmail.com>
2022-08-20 18:26:01 +0600

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+    return  HomePage();
   }
 
 }
