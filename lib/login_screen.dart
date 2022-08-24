280f8b92ba6f96f51d25214ccf3176afe101f77f
Nahin-CDR <dev.nahin95@gmail.com>
2022-08-24 18:01:03 +0600

    unnecessary line removed

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,8 +1,6 @@
 import 'package:api_consume/homePage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-
 class LoginWidget extends StatefulWidget {
   const LoginWidget({Key? key}) : super(key: key);
 
